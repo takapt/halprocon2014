ac16a4e9942c6e0062027a4fa9bbb113728431ff
takapt <takaptpro@gmail.com>
2014-10-27 16:19:59 +0900

    temp

--- Answer.cpp
+++ Answer.cpp
@@ -11,56 +11,46 @@ using namespace hpc;
 namespace hpc
 {
 int st = -1;
-    void Answer::Init(const StageAccessor& aStageAccessor)
-    {
-        ++st;
-//         printf("%.3f, %.3f\n", aStageAccessor.field().rect().width(), aStageAccessor.field().rect().height());
-    }
+void Answer::Init(const StageAccessor& aStageAccessor)
+{
+    ++st;
+    //         printf("%.3f, %.3f\n", aStageAccessor.field().rect().width(), aStageAccessor.field().rect().height());
+}
 
-    Action Answer::GetNextAction(const StageAccessor& aStageAccessor)
-    {
-        const Chara& player = aStageAccessor.player();
-        const LotusCollection& lotuses = aStageAccessor.lotuses();
-        const Field& field = aStageAccessor.field();
+Action Answer::GetNextAction(const StageAccessor& aStageAccessor)
+{
+    const Chara& player = aStageAccessor.player();
+    const LotusCollection& lotuses = aStageAccessor.lotuses();
+    const Field& field = aStageAccessor.field();
 
-        const Lotus& lotus = lotuses[player.targetLotusNo()];
-        const Vec2& next = lotus.pos();
-//         const Vec2 vec = next - player.pos();
-        const Vec2& nextnext = lotuses[(player.targetLotusNo() + 1) % aStageAccessor.lotuses().count()].pos();
-        const Vec2 target = next + (nextnext - next).getNormalized(lotus.radius() * 0.9);
-        Vec2 vec = target - player.pos();
+    const Lotus& lotus = lotuses[player.targetLotusNo()];
+    const Vec2& next = lotus.pos();
+    const Vec2& nextnext = lotuses[(player.targetLotusNo() + 1) % aStageAccessor.lotuses().count()].pos();
+    const Vec2 target = next + (nextnext - next).getNormalized(lotus.radius() * 0.9);
+    Vec2 vec = target - player.pos();
 
-//         if (st == 94 && abs(player.passedTurn() - 590) < 6)
-//         {
-//             dump(player.targetLotusNo());
-//             dump(player.accelCount());
-//             dump(player.vel().getProjected(vec).length());
-//             fprintf(stderr, "(%.3f, %.3f), (%.3f, %.3f)\n", player.vel().x, player.vel().y, vec.x, vec.y);
-//             cerr << endl;
-//         }
-
-        if (player.accelCount() == 0)
-            return Action::Wait();
-        else if (vec.length() > 1.5 && player.accelCount() >= Parameter::CharaAccelCountMax)
-            return Action::Accel(target - vec.length() * aStageAccessor.field().flowVel());
-
-        if ((player.vel() + field.flowVel()).dot(vec) / vec.length() > 0.19)
-            return Action::Wait();
-
-        {
-            Chara p = player;
-            int turns = 15;
-            if (vec.dot(aStageAccessor.field().flowVel()) > 0.4)
-                turns = 40;
-
-            rep(t, turns)
-            {
-                if (Collision::IsHit(p.region(), lotus.region()))
-                    return Action::Wait();
-                p.move();
-            }
-        }
-
+    if (player.accelCount() == 0)
+        return Action::Wait();
+    else if (vec.length() > 1.5 && player.accelCount() >= Parameter::CharaAccelCountMax)
         return Action::Accel(target - vec.length() * aStageAccessor.field().flowVel());
+
+    if ((player.vel() + field.flowVel()).dot(vec) / vec.length() > 0.19)
+        return Action::Wait();
+
+    {
+        Chara p = player;
+        int turns = 15;
+        if (vec.dot(aStageAccessor.field().flowVel()) > 0.4)
+            turns = 40;
+
+        rep(t, turns)
+        {
+            if (Collision::IsHit(p.region(), lotus.region()))
+                return Action::Wait();
+            p.move();
+        }
     }
+
+    return Action::Accel(target - vec.length() * aStageAccessor.field().flowVel());
+}
 }
