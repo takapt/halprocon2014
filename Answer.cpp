04d4c14c510df1e6e8fa6f375124725f6ee94d1d
takapt <takaptpro@gmail.com>
2014-10-27 14:54:37 +0900

    3.9m

--- Answer.cpp
+++ Answer.cpp
@@ -10,8 +10,10 @@ using namespace hpc;
 
 namespace hpc
 {
+int st = -1;
     void Answer::Init(const StageAccessor& aStageAccessor)
     {
+        ++st;
 //         printf("%.3f, %.3f\n", aStageAccessor.field().rect().width(), aStageAccessor.field().rect().height());
     }
 
@@ -27,15 +29,24 @@ namespace hpc
         const Vec2& nextnext = lotuses[(player.targetLotusNo() + 1) % aStageAccessor.lotuses().count()].pos();
         const Vec2 target = next + (nextnext - next).getNormalized(lotus.radius() * 0.9);
 
+//         if (st == 94 && abs(player.passedTurn() - 590) < 6)
+//         {
+//             dump(player.targetLotusNo());
+//             dump(player.accelCount());
+//             dump(player.vel().getProjected(vec).length());
+//             fprintf(stderr, "(%.3f, %.3f), (%.3f, %.3f)\n", player.vel().x, player.vel().y, vec.x, vec.y);
+//             cerr << endl;
+//         }
+
         if (player.accelCount() == 0)
             return Action::Wait();
 
-        if (player.vel().getProjected(vec).length() > 0.3)
+        if (player.vel().dot(vec) / vec.length() > 0.23)
             return Action::Wait();
 
         {
             Chara p = player;
-            int turns = 20;
+            int turns = 15;
             if (vec.dot(aStageAccessor.field().flowVel()) > 0.4)
                 turns = 50;
             rep(t, turns)
